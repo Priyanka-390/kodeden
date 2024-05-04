7dd6476a7e95fab03d7737124ab1c3dac6f7d63f
Priyanka-390 <psr82220@gmail.com>
2024-05-04 10:09:15 +0530

    update

--- src/components/Mynav.jsx
+++ src/components/Mynav.jsx
@@ -1,35 +1,43 @@
-"use client"
+"use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Navdata } from "./Mapdata";
 
 export default function Mynav() {
   const [show, setshow] = useState(true);
-  if (show === false) {
-    document.body.classList.add("overflow_hidden");
-  } else {
-    document.body.classList.remove("overflow_hidden");
-  }
+  useEffect(() => {
+    if (show === false) {
+      document.body.classList.add("overflow_hidden");
+    } else {
+      document.body.classList.remove("overflow_hidden");
+    }
+  });
   return (
     <div className="d-flex justify-content-center pt-3 align-items-center">
-          <div className="container">
-              <div className="d-flex justify-content-end">
-        <ul className={`${show ? "left_100" : "left_0"}  nav_bar ps-0 mb-0`}>
-          {Navdata.map((data, i) => {
-            return (
-              <li onClick={() => setshow(!show)} key={i}>
-                <a
-                  href={data.path}
-                  className="ff-jost navlink fw-medium text-decoration-none fs-16 text-grey lh-23 "
-                >
-                  {data.link}
-                </a>
+      <div className="container">
+        <div className="d-flex justify-content-end">
+          <ul className={`${show ? "left_100" : "left_0"}  nav_bar ps-0 mb-0`}>
+            {Navdata.map((data, i) => {
+              return (
+                <li onClick={() => setshow(!show)} key={i}>
+                  <a
+                    href={data.path}
+                    className="ff-jost navlink fw-medium text-decoration-none fs-16 text-grey lh-23 "
+                  >
+                    {data.link}
+                  </a>
                 </li>
-            );
-          })}
-                  <li><a href=""><button className="fs-14 navbtn px-4 text-lightwhite fw-900">Get Started</button></a></li>
-              </ul>
-               <div
+              );
+            })}
+            <li>
+              <a href="">
+                <button className="fs-14 navbtn px-4 text-lightwhite fw-900">
+                  Get Started
+                </button>
+              </a>
+            </li>
+          </ul>
+          <div
             onClick={() => setshow(!show)}
             className={`${show ? "" : "cross"} navline`}
           >
@@ -37,8 +45,8 @@ export default function Mynav() {
             <span className="crl-2"></span>
             <span className="crl-3"></span>
           </div>
-              </div>
-              </div>
+        </div>
+      </div>
     </div>
   );
 }
